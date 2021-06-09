b85a837fa8fa767ca94b60eeeef6d5ef4e4bea7f
Austin Przybysz <austpryb@gmail.com>
2021-06-09 09:35:14 -0400

    test

--- azure/main.tf
+++ azure/main.tf
@@ -31,8 +31,10 @@ resource "azurerm_kubernetes_cluster" "azcluster" {
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.azcluster.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.azcluster.kube_config_raw
+  sensitive = true
 }
